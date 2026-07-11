1614bff73324b41b6a440c01b63f72b7041eb7b5
Linwy <91447283+linwys@users.noreply.github.com>
2026-07-12 00:16:29 +0300

    Update library_view.hpp

--- src/ui/library_view.hpp
+++ src/ui/library_view.hpp
@@ -1,11 +1,14 @@
 #pragma once
 #include <QWidget>
-#include <QTableWidget>
+#include <QTableView>
 #include <QPushButton>
 #include <QLabel>
+#include <QLineEdit>
 #include <QVBoxLayout>
 #include <QHBoxLayout>
+#include <vector>
 #include "playlist_view.hpp"
+#include "track_model.hpp"
 #include "player/db.hpp"
 
 namespace ui {
@@ -17,16 +20,24 @@ public:
     ~LibraryView() override = default;
 
     void refresh();
+    void toggle_search();
+    std::vector<player::Track> current_queue() const;
 
 signals:
     void play_requested(const std::vector<player::Track>& queue, size_t index);
+    void queue_updated(const std::vector<player::Track>& queue);
+
+protected:
+    void keyPressEvent(QKeyEvent* event) override;
 
 private:
     void setup_ui();
     void handle_favorite_changed(const QUuid& id, bool is_favorite);
 
     player::Db* m_db{nullptr};
-    QTableWidget* m_table{nullptr};
+    QTableView* m_table{nullptr};
+    TrackModel* m_model{nullptr};
+    QLineEdit* m_search_bar{nullptr};
     QLabel* m_header_label{nullptr};
     QLabel* m_sub_label{nullptr};
     TrackDelegate* m_delegate{nullptr};
